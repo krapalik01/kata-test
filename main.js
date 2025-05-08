a73b3c692f8f0094ae1afed911826f7fe9f8ee7d
krap <krapalik03@mail.ru>
2025-05-08 13:34:39 +0300

    third  commit

--- main.js
+++ main.js
@@ -9,38 +9,18 @@ keyboard: true,
 });
 
 
-const img1 = document.querySelector('.cards-button__img_768px')
-const img2 = document.querySelector('.cards-button__img_1120px')
-const text1 = document.getElementById('text1')
-const text2 = document.getElementById('text2')
-const button1 = document.querySelector('.cards-button_768px')
-const button2 = document.querySelector('.cards-button_1120px')
+const img = document.querySelector('.cards-button__img')
+const text = document.getElementById('text')
+const button1 = document.querySelector('.cards-button')
 
 
 button1.onclick = function() {
-    const elements = document.querySelectorAll('.firstHiden');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle('active')
-    };
-    button2.onclick()
-    img1.classList.toggle('cards-button__img_active')
-    if (img1.classList.contains('cards-button__img_active')) {
-        text1.textContent = 'Скрыть' 
+    const elements = document.getElementById('active');
+        elements.classList.toggle('cards_active')
+    img.classList.toggle('cards-button__img_active')
+    if (img.classList.contains('cards-button__img_active')) {
+        text.textContent = 'Скрыть' 
     } else {
-        text1.textContent = 'Показать всё' 
-    }
-};
-
-button2.onclick = function() {
-    const elements = document.querySelectorAll('.secondHiden');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle('active')
-    };
-    img2.classList.toggle('cards-button__img_active')
-    
-    if (img2.classList.contains('cards-button__img_active')) {
-        text2.textContent = 'Скрыть' 
-    } else {
-        text2.textContent = 'Показать всё' 
+        text.textContent = 'Показать всё' 
     }
 };
